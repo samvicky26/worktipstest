583f9a0759ff79287400aad3135aeacb2444320b
samvicky <billavignesh26@gmail.com>
2018-04-06 17:09:39 +0530

    v1

---
+++ src/version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern const char* const WORKTIPS_VERSION_TAG;
+extern const char* const WORKTIPS_VERSION;
+extern const char* const WORKTIPS_RELEASE_NAME;
+extern const char* const WORKTIPS_VERSION_FULL;
